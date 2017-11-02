ce53fa4b4c35302697933dd18db271234c0171d8
Kyuho Kim <valve.god.project@gmail.com>
2017-11-02 20:39:32 +0900

    Create connected_sensor.ino

---
+++ connected_sensor.ino
@@ -0,0 +1,68 @@
+const char* host = "your_ip";  //"api.thingspeak.com";
+String url = "/update?api_key=your_api_key";  
+const int httpPort = 5000; //80;
+int interval = 1000;
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+const char* ssid = "Seoul IOT_2(2.4G)";
+const char* password = "12345678";
+
+String working() { 
+  int r = get_co2(); // do some magic here!
+  return(String("field1=")+String(r));
+}
+
+void delivering(String payload) { 
+  WiFiClient client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, httpPort)) {
+    Serial.print("connection failed: ");
+    Serial.println(payload);
+    return;
+  }
+
+  String getheader = "GET "+ String(url) +"&"+ String(payload) +" HTTP/1.1";
+  client.println(getheader);
+  client.println("User-Agent: ESP8266 Kyuho Kim");  
+  client.println("Host: " + String(host));  
+  client.println("Connection: close");  
+  client.println();
+
+  Serial.println(getheader);
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    Serial.println(line);
+  }
+  Serial.println("Done cycle.");
+}
+
+void connect_ap() {
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\n Got WiFi, IP address: ");
+  Serial.println(WiFi.localIP());  
+}
+
+void setup() {
+  Serial.begin(115200);
+  connect_ap();
+  Serial.println("ESPArduinoThingSpeak.cpp - 2017/11/2");
+}
+
+unsigned long mark = 0;
+void loop() {
+  if (millis() > mark ) {
+     mark = millis() + interval;
+     String payload = working();
+     delivering(payload);
+  }
+}
